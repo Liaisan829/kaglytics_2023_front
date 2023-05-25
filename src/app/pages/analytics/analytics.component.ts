008236005b16cc1411cf88012e77f3fb3fd75a66
Liaisan829 <79139067+Liaisan829@users.noreply.github.com>
2023-05-26 01:54:54 +0300

    fixed loading and charts

--- src/app/pages/analytics/analytics.component.ts
+++ src/app/pages/analytics/analytics.component.ts
@@ -3,6 +3,7 @@ import { EChartsOption } from "echarts";
 import { AnalyticsService } from "@services/analytics.service";
 import { takeUntil } from "rxjs";
 import { DestroyService } from "@services/destroy.service";
+import { ToastService } from "@services/toast.service";
 
 @Component({
 	selector: 'app-analytics',
@@ -21,12 +22,16 @@ export class AnalyticsComponent implements OnInit {
 	rewardTypesOptions: EChartsOption;
 	tagsOptions: EChartsOption;
 
-	loading: boolean = false;
+	loadCategories = true;
+	loadOrganizations = true;
+	loadRewardTypes = true;
+	loadTags: boolean = true;
 
 	constructor(
 		private analyticsService: AnalyticsService,
 		private destroy$: DestroyService,
 		private cdr: ChangeDetectorRef,
+		private toast: ToastService
 	) {
 		this.categoriesOptions = {};
 		this.organizationOptions = {};
@@ -37,135 +42,278 @@ export class AnalyticsComponent implements OnInit {
 	ngOnInit() {
 		this.analyticsService.getCategoriesStatistics()
 			.pipe(takeUntil(this.destroy$))
-			.subscribe(res => {
-				this.categories = Object.entries(res).map(([key, value]) => ({key, value}));
-				this.cdr.markForCheck();
-				this.categoriesChart();
+			.subscribe({
+				next: (res) => {
+					this.categories = Object.entries(res).map(([key, value]) => ({key, value}));
+					this.cdr.markForCheck();
+					this.categoriesChart();
+				},
+				error: () => {
+					this.loadCategories = false;
+					this.toast.error('An error has occured');
+				},
+				complete: () => {
+					this.loadCategories = false;
+				}
 			});
 
 		this.analyticsService.getOrganizationsStatistics()
 			.pipe(takeUntil(this.destroy$))
-			.subscribe(res => {
-				this.organizations = Object.entries(res).map(([key, value]) => ({key, value}));
-				this.cdr.markForCheck();
-				this.organizationsChart();
+			.subscribe({
+				next: (res) => {
+					this.organizations = Object.entries(res).map(([key, value]) => ({key, value}));
+					this.cdr.markForCheck();
+					this.organizationsChart();
+				},
+				error: () => {
+					this.loadOrganizations = false;
+					this.toast.error('An error has occured');
+				},
+				complete: () => {
+					this.loadOrganizations = false;
+				}
 			});
 
-		// this.analyticsService.getRewardTypesStatistics()
-		//   .pipe(takeUntil(this.destroy$))
-		//   .subscribe(res => {
-		//     console.log(res);
-		//   });
+		this.analyticsService.getRewardTypesStatistics()
+			.pipe(takeUntil(this.destroy$))
+			.subscribe({
+				next: (res) => {
+					this.rewardTypes = Object.entries(res).map(([key, value]) => ({key, value}));
+					this.cdr.markForCheck();
+					this.rewardTypesChart();
+				},
+				error: () => {
+					this.loadRewardTypes = false;
+					this.toast.error('An error has occured');
+				},
+				complete: () => {
+					this.loadRewardTypes = false;
+				}
+			});
 
 		this.analyticsService.getTagsStatistics()
 			.pipe(takeUntil(this.destroy$))
-			.subscribe(res => {
-				this.tags = Object.entries(res).map(([key, value]) => ({key, value}));
-				this.cdr.markForCheck();
-				this.tagsChart();
+			.subscribe({
+				next: (res) => {
+					this.tags = Object.entries(res).map(([key, value]) => ({key, value}));
+					this.cdr.markForCheck();
+					this.tagsChart();
+				},
+				error: () => {
+					this.loadTags = false;
+					this.toast.error('An error has occured');
+				},
+				complete: () => {
+					this.loadTags = false;
+				}
 			});
-
-		// setTimeout(() => {
-		// 	this.loading = false;
-		// 	this.cdr.markForCheck();
-		// }, 6000);
 	}
+
 
 	categoriesChart() {
 		this.categoriesOptions = {
+			title: {
+				text: 'Categories',
+				textStyle: {
+					fontSize: 22
+				}
+			},
 			legend: {
 				show: false
 			},
+			grid: {
+				left: '3%',
+				right: '4%',
+				bottom: '5%',
+				containLabel: true
+			},
 			tooltip: {},
-			xAxis: {
+			xAxis: [{
 				type: 'category',
 				data: [this.categories[0].key, this.categories[1].key, this.categories[2].key, this.categories[3].key, this.categories[4].key, this.categories[5].key, this.categories[6].key],
+				axisTick: {
+					alignWithLabel: true
+				},
 				axisLabel: {
-					fontSize: 14,
-					color: '#000',
-					fontWeight: 'normal'
-				}
-			},
+					show: true,
+					rotate: 45,
+				},
+			}],
 			yAxis: {
 				type: 'value',
-				// max: 500,
 			},
 			series: [
-				{type: 'bar', data: [this.categories[0].value, , , , , ,]},
-				{type: 'bar', data: [, this.categories[1].value, , , , ,]},
-				{type: 'bar', data: [, , this.categories[2].value, , , ,]},
-				{type: 'bar', data: [, , , this.categories[3].value, , ,]},
-				{type: 'bar', data: [, , , , this.categories[4].value, ,]},
-				{type: 'bar', data: [, , , , , this.categories[5].value,]},
-				{type: 'bar', data: [, , , , , , this.categories[6].value]}
+				{
+					type: 'bar',
+					barWidth: 20,
+					data: [this.categories[0].value, , , , , ,]
+				},
+				{
+					type: 'bar',
+					barWidth: 20,
+					data: [, this.categories[1].value, , , , ,]
+				},
+				{
+					type: 'bar',
+					barWidth: 20,
+					data: [, , this.categories[2].value, , , ,]
+				},
+				{
+					type: 'bar',
+					barWidth: 20,
+					data: [, , , this.categories[3].value, , ,]
+				},
+				{
+					type: 'bar',
+					barWidth: 20,
+					data: [, , , , this.categories[4].value, ,]
+				},
+				{
+					type: 'bar',
+					barWidth: 20,
+					data: [, , , , , this.categories[5].value,]
+				},
+				{
+					type: 'bar',
+					barWidth: 20,
+					data: [, , , , , , this.categories[6].value]
+				}
 			],
 		}
 	};
 
 	organizationsChart() {
 		this.organizationOptions = {
+			title: {
+				text: 'Organizations',
+				textStyle: {
+					fontSize: 22
+				}
+			},
 			legend: {
 				show: false
+			},
+			grid: {
+				left: '3%',
+				right: '4%',
+				bottom: '5%',
+				containLabel: true
 			},
 			tooltip: {},
 			xAxis: {
 				type: 'category',
 				data: [this.organizations[0].key, this.organizations[1].key, this.organizations[2].key, this.organizations[3].key, this.organizations[4].key, this.organizations[5].key, this.organizations[6].key, this.organizations[7].key, this.organizations[8].key, this.organizations[9].key],
+				axisTick: {
+					alignWithLabel: true
+				},
 				axisLabel: {
-					fontSize: 14,
-					color: '#000',
-					fontWeight: 'normal'
-				}
+					show: true,
+					rotate: 45,
+				},
 			},
 			yAxis: {
 				type: 'value',
-				// max: 500,
 			},
 			series: [
-				{type: 'bar', data: [this.organizations[0].value, , , , , , , , ,]},
-				{type: 'bar', data: [, this.organizations[1].value, , , , , , , ,]},
-				{type: 'bar', data: [, , this.organizations[2].value, , , , , , ,]},
-				{type: 'bar', data: [, , , this.organizations[3].value, , , , , ,]},
-				{type: 'bar', data: [, , , , this.organizations[4].value, , , , ,]},
-				{type: 'bar', data: [, , , , , this.organizations[5].value, , , ,]},
-				{type: 'bar', data: [, , , , , , this.organizations[6].value, , ,]},
-				{type: 'bar', data: [, , , , , , , this.organizations[7].value, ,]},
-				{type: 'bar', data: [, , , , , , , , this.organizations[8].value,]},
-				{type: 'bar', data: [, , , , , , , , , this.organizations[9].value]}
+				{type: 'bar', barWidth: 30, data: [this.organizations[0].value, , , , , , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, this.organizations[1].value, , , , , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , this.organizations[2].value, , , , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , this.organizations[3].value, , , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , this.organizations[4].value, , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , this.organizations[5].value, , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , , this.organizations[6].value, , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , , , this.organizations[7].value, ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , , , , this.organizations[8].value,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , , , , , this.organizations[9].value]}
+			],
+		};
+	}
+
+	rewardTypesChart() {
+		this.rewardTypesOptions = {
+			title: {
+				text: 'Reward types',
+				textStyle: {
+					fontSize: 22
+				}
+			},
+			legend: {
+				show: false
+			},
+			grid: {
+				left: '3%',
+				right: '4%',
+				bottom: '5%',
+				containLabel: true
+			},
+			tooltip: {},
+			xAxis: {
+				type: 'category',
+				data: [this.rewardTypes[0].key, this.rewardTypes[1].key, this.rewardTypes[2].key, this.rewardTypes[3].key, this.rewardTypes[4].key, this.rewardTypes[5].key, this.rewardTypes[6].key, this.rewardTypes[7].key],
+				axisTick: {
+					alignWithLabel: true
+				},
+				axisLabel: {
+					show: true,
+					rotate: 45,
+				},
+			},
+			yAxis: {
+				type: 'value',
+			},
+			series: [
+				{type: 'bar', barWidth: 30, data: [this.rewardTypes[0].value, , , , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, this.rewardTypes[1].value, , , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , this.rewardTypes[2].value, , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , this.rewardTypes[3].value, , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , this.rewardTypes[4].value, , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , this.rewardTypes[5].value, ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , , this.rewardTypes[6].value,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , , , this.rewardTypes[7].value]},
 			],
 		};
 	}
 
 	tagsChart() {
 		this.tagsOptions = {
+			title: {
+				text: 'Tags',
+				textStyle: {
+					fontSize: 22
+				}
+			},
 			legend: {
 				show: false
+			},
+			grid: {
+				left: '3%',
+				right: '4%',
+				bottom: '5%',
+				containLabel: true
 			},
 			tooltip: {},
 			xAxis: {
 				type: 'category',
 				data: [this.tags[0].key, this.tags[1].key, this.tags[2].key, this.tags[3].key, this.tags[4].key, this.tags[5].key, this.tags[6].key, this.tags[7].key, this.tags[8].key, this.tags[9].key],
 				axisLabel: {
-					fontSize: 14,
-					color: '#000',
-					fontWeight: 'normal'
-				}
+					show: true,
+					rotate: 45,
+				},
 			},
 			yAxis: {
 				type: 'value',
-				// max: 500,
 			},
 			series: [
-				{type: 'bar', data: [this.tags[0].value, , , , , , , , ,]},
-				{type: 'bar', data: [, this.tags[1].value, , , , , , , ,]},
-				{type: 'bar', data: [, , this.tags[2].value, , , , , , ,]},
-				{type: 'bar', data: [, , , this.tags[3].value, , , , , ,]},
-				{type: 'bar', data: [, , , , this.tags[4].value, , , , ,]},
-				{type: 'bar', data: [, , , , , this.tags[5].value, , , ,]},
-				{type: 'bar', data: [, , , , , , this.tags[6].value, , ,]},
-				{type: 'bar', data: [, , , , , , , this.tags[7].value, ,]},
-				{type: 'bar', data: [, , , , , , , , this.tags[8].value,]},
-				{type: 'bar', data: [, , , , , , , , , this.tags[9].value]}
+				{type: 'bar', barWidth: 30, data: [this.tags[0].value, , , , , , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, this.tags[1].value, , , , , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , this.tags[2].value, , , , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , this.tags[3].value, , , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , this.tags[4].value, , , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , this.tags[5].value, , , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , , this.tags[6].value, , ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , , , this.tags[7].value, ,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , , , , this.tags[8].value,]},
+				{type: 'bar', barWidth: 30, data: [, , , , , , , , , this.tags[9].value]}
 			],
 		}
 	}
