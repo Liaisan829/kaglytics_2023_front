3369bfe6e84901a1e822d685222288dde4d75c39
terentyevaolga <terentyeevaolga@gmail.com>
2023-05-25 08:20:45 +0300

    added Сategories with some bugs

--- src/app/pages/analytics/analytics.component.ts
+++ src/app/pages/analytics/analytics.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { EChartsOption } from "echarts";
+import { AnalyticsService } from "@services/analytics.service";
+import { takeUntil } from "rxjs";
+import { DestroyService } from "@services/destroy.service";
 
 @Component({
 	selector: 'app-analytics',
@@ -7,28 +10,63 @@ import { EChartsOption } from "echarts";
 	styleUrls: ['./analytics.component.sass'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AnalyticsComponent {
+export class AnalyticsComponent implements OnInit {
+	categories = {};
+	organizations = {};
+	rewardTypes = {};
+	tags = {};
 
-	options: EChartsOption = {
-		legend: {},
-		tooltip: {},
-		dataset: {
-			// Provide a set of data.
-			source: [
-				['product', '2015', '2016', '2017'],
-				['Matcha Latte', 43.3, 85.8, 93.7],
-				['Milk Tea', 83.1, 73.4, 55.1],
-				['Cheese Cocoa', 86.4, 65.2, 82.5],
-				['Walnut Brownie', 72.4, 53.9, 39.1],
+	categoriesOptions: EChartsOption;
+
+	constructor(
+		private analyticsService: AnalyticsService,
+		private destroy$: DestroyService,
+	    private cdr: ChangeDetectorRef
+	) {
+		this.categoriesOptions = {};
+	}
+
+	ngOnInit() {
+		this.analyticsService.getCategoriesStatistics()
+			.pipe(takeUntil(this.destroy$))
+			.subscribe(res => {
+				this.categories = Object.entries(res).map(([key, value]) => ({key, value}));
+				this.cdr.markForCheck();
+				this.categoriesChart();
+			});
+	}
+
+	categoriesChart() {
+		this.categoriesOptions = {
+			legend: {
+				show: false
+			},
+			tooltip: {},
+			xAxis: {
+				type: 'category',
+				data: [this.categories[0].key, this.categories[1].key, this.categories[2].key, this.categories[3].key, this.categories[4].key, this.categories[5].key, this.categories[6].key],
+				axisLabel: {
+					fontSize: 14,
+					color: '#000',
+					fontWeight: 'normal'
+				}
+			},
+			// Declare a y-axis (value axis).
+			yAxis: {
+				type: 'value',
+				// max: 500,
+			},
+			// Declare several 'bar' series,
+			// every series will auto-map to each column by default.
+			series: [
+				{type: 'bar', data: [this.categories[0].value, , , , , ,]},
+				{type: 'bar', data: [, this.categories[1].value, , , , ,]},
+				{type: 'bar', data: [, , this.categories[2].value, , , ,]},
+				{type: 'bar', data: [, , , this.categories[3].value, , ,]},
+				{type: 'bar', data: [, , , , this.categories[4].value, ,]},
+				{type: 'bar', data: [, , , , , this.categories[5].value,]},
+				{type: 'bar', data: [, , , , , , this.categories[6].value]}
 			],
-		},
-		// Declare an x-axis (category axis).
-		// The category map the first column in the dataset by default.
-		xAxis: {type: 'category'},
-		// Declare a y-axis (value axis).
-		yAxis: {},
-		// Declare several 'bar' series,
-		// every series will auto-map to each column by default.
-		series: [{type: 'bar'}, {type: 'bar'}, {type: 'bar'}],
+		}
 	};
 }
