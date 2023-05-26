28981bc9f9a56c98c05c179973dd06316d83154d
terentyevaolga <terentyeevaolga@gmail.com>
2023-05-26 21:30:37 +0300

    changing charts

--- src/app/pages/analytics/analytics.component.ts
+++ src/app/pages/analytics/analytics.component.ts
@@ -114,207 +114,144 @@ export class AnalyticsComponent implements OnInit {
 		this.categoriesOptions = {
 			title: {
 				text: 'Categories',
-				textStyle: {
-					fontSize: 22
-				}
+				left: 'left'
 			},
-			legend: {
-				show: false
-			},
-			grid: {
-				left: '3%',
-				right: '4%',
-				bottom: '5%',
-				containLabel: true
-			},
-			tooltip: {},
-			xAxis: [{
-				type: 'category',
-				data: [this.categories[0].key, this.categories[1].key, this.categories[2].key, this.categories[3].key, this.categories[4].key, this.categories[5].key, this.categories[6].key],
-				axisTick: {
-					alignWithLabel: true
-				},
-				axisLabel: {
-					show: true,
-					rotate: 45,
-				},
-			}],
-			yAxis: {
-				type: 'value',
+			tooltip: {
+				trigger: 'item'
 			},
 			series: [
 				{
-					type: 'bar',
-					barWidth: 20,
-					data: [this.categories[0].value, , , , , ,]
-				},
-				{
-					type: 'bar',
-					barWidth: 20,
-					data: [, this.categories[1].value, , , , ,]
-				},
-				{
-					type: 'bar',
-					barWidth: 20,
-					data: [, , this.categories[2].value, , , ,]
-				},
-				{
-					type: 'bar',
-					barWidth: 20,
-					data: [, , , this.categories[3].value, , ,]
-				},
-				{
-					type: 'bar',
-					barWidth: 20,
-					data: [, , , , this.categories[4].value, ,]
-				},
-				{
-					type: 'bar',
-					barWidth: 20,
-					data: [, , , , , this.categories[5].value,]
-				},
-				{
-					type: 'bar',
-					barWidth: 20,
-					data: [, , , , , , this.categories[6].value]
+					type: 'pie',
+					radius: '90%',
+					data: [
+						{value: this.categories[0].value, name: this.categories[0].key},
+						{value: this.categories[1].value, name: this.categories[1].key},
+						{value: this.categories[2].value, name: this.categories[2].key},
+						{value: this.categories[3].value, name: this.categories[3].key},
+						{value: this.categories[4].value, name: this.categories[4].key},
+						{value: this.categories[5].value, name: this.categories[5].key},
+						{value: this.categories[6].value, name: this.categories[6].key},
+					],
+					emphasis: {
+						itemStyle: {
+							shadowBlur: 10,
+							shadowOffsetX: 0,
+							shadowColor: 'rgba(0, 0, 0, 0.5)'
+						}
+					}
 				}
-			],
+			]
 		}
 	};
+
 
 	organizationsChart() {
 		this.organizationOptions = {
 			title: {
 				text: 'Organizations',
-				textStyle: {
-					fontSize: 22
-				}
+				left: 'left'
 			},
-			legend: {
-				show: false
-			},
-			grid: {
-				left: '3%',
-				right: '4%',
-				bottom: '5%',
-				containLabel: true
-			},
-			tooltip: {},
-			xAxis: {
-				type: 'category',
-				data: [this.organizations[0].key, this.organizations[1].key, this.organizations[2].key, this.organizations[3].key, this.organizations[4].key, this.organizations[5].key, this.organizations[6].key, this.organizations[7].key, this.organizations[8].key, this.organizations[9].key],
-				axisTick: {
-					alignWithLabel: true
-				},
-				axisLabel: {
-					show: true,
-					rotate: 45,
-				},
-			},
-			yAxis: {
-				type: 'value',
+			tooltip: {
+				trigger: 'item'
 			},
 			series: [
-				{type: 'bar', barWidth: 30, data: [this.organizations[0].value, , , , , , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, this.organizations[1].value, , , , , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , this.organizations[2].value, , , , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , this.organizations[3].value, , , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , this.organizations[4].value, , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , this.organizations[5].value, , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , , this.organizations[6].value, , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , , , this.organizations[7].value, ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , , , , this.organizations[8].value,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , , , , , this.organizations[9].value]}
-			],
+				{
+					type: 'pie',
+					radius: '90%',
+					data: [
+						{value: this.organizations[0].value, name: this.organizations[0].key},
+						{value: this.organizations[1].value, name: this.organizations[1].key},
+						{value: this.organizations[2].value, name: this.organizations[2].key},
+						{value: this.organizations[3].value, name: this.organizations[3].key},
+						{value: this.organizations[4].value, name: this.organizations[4].key},
+						{value: this.organizations[5].value, name: this.organizations[5].key},
+						{value: this.organizations[6].value, name: this.organizations[6].key},
+						{value: this.organizations[7].value, name: this.organizations[7].key},
+						{value: this.organizations[8].value, name: this.organizations[8].key},
+						{value: this.organizations[9].value, name: this.organizations[9].key},
+					],
+					emphasis: {
+						itemStyle: {
+							shadowBlur: 10,
+							shadowOffsetX: 0,
+							shadowColor: 'rgba(0, 0, 0, 0.5)'
+						}
+					}
+				}
+			]
 		};
 	}
 
 	rewardTypesChart() {
 		this.rewardTypesOptions = {
 			title: {
-				text: 'Reward types',
-				textStyle: {
-					fontSize: 22
-				}
+				text: 'Reward Types',
+				left: 'left'
 			},
-			legend: {
-				show: false
-			},
-			grid: {
-				left: '3%',
-				right: '4%',
-				bottom: '5%',
-				containLabel: true
-			},
-			tooltip: {},
-			xAxis: {
-				type: 'category',
-				data: [this.rewardTypes[0].key, this.rewardTypes[1].key, this.rewardTypes[2].key, this.rewardTypes[3].key, this.rewardTypes[4].key, this.rewardTypes[5].key, this.rewardTypes[6].key, this.rewardTypes[7].key],
-				axisTick: {
-					alignWithLabel: true
-				},
-				axisLabel: {
-					show: true,
-					rotate: 45,
-				},
-			},
-			yAxis: {
-				type: 'value',
+			tooltip: {
+				trigger: 'item'
 			},
 			series: [
-				{type: 'bar', barWidth: 30, data: [this.rewardTypes[0].value, , , , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, this.rewardTypes[1].value, , , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , this.rewardTypes[2].value, , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , this.rewardTypes[3].value, , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , this.rewardTypes[4].value, , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , this.rewardTypes[5].value, ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , , this.rewardTypes[6].value,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , , , this.rewardTypes[7].value]},
-			],
+				{
+					type: 'pie',
+					radius: '90%',
+					data: [
+						{value: this.rewardTypes[0].value, name: this.rewardTypes[0].key},
+						{value: this.rewardTypes[1].value, name: this.rewardTypes[1].key},
+						{value: this.rewardTypes[2].value, name: this.rewardTypes[2].key},
+						{value: this.rewardTypes[3].value, name: this.rewardTypes[3].key},
+						{value: this.rewardTypes[4].value, name: this.rewardTypes[4].key},
+						{value: this.rewardTypes[5].value, name: this.rewardTypes[5].key},
+						{value: this.rewardTypes[6].value, name: this.rewardTypes[6].key},
+						{value: this.rewardTypes[7].value, name: this.rewardTypes[7].key},
+					],
+					emphasis: {
+						itemStyle: {
+							shadowBlur: 10,
+							shadowOffsetX: 0,
+							shadowColor: 'rgba(0, 0, 0, 0.5)'
+						}
+					}
+				}
+			]
 		};
 	}
+
 
 	tagsChart() {
 		this.tagsOptions = {
 			title: {
 				text: 'Tags',
-				textStyle: {
-					fontSize: 22
-				}
+				left: 'left'
 			},
-			legend: {
-				show: false
-			},
-			grid: {
-				left: '3%',
-				right: '4%',
-				bottom: '5%',
-				containLabel: true
-			},
-			tooltip: {},
-			xAxis: {
-				type: 'category',
-				data: [this.tags[0].key, this.tags[1].key, this.tags[2].key, this.tags[3].key, this.tags[4].key, this.tags[5].key, this.tags[6].key, this.tags[7].key, this.tags[8].key, this.tags[9].key],
-				axisLabel: {
-					show: true,
-					rotate: 45,
-				},
-			},
-			yAxis: {
-				type: 'value',
+			tooltip: {
+				trigger: 'item'
 			},
 			series: [
-				{type: 'bar', barWidth: 30, data: [this.tags[0].value, , , , , , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, this.tags[1].value, , , , , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , this.tags[2].value, , , , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , this.tags[3].value, , , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , this.tags[4].value, , , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , this.tags[5].value, , , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , , this.tags[6].value, , ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , , , this.tags[7].value, ,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , , , , this.tags[8].value,]},
-				{type: 'bar', barWidth: 30, data: [, , , , , , , , , this.tags[9].value]}
-			],
+				{
+					type: 'pie',
+					radius: '90%',
+					data: [
+						{value: this.tags[0].value, name: this.tags[0].key},
+						{value: this.tags[1].value, name: this.tags[1].key},
+						{value: this.tags[2].value, name: this.tags[2].key},
+						{value: this.tags[3].value, name: this.tags[3].key},
+						{value: this.tags[4].value, name: this.tags[4].key},
+						{value: this.tags[5].value, name: this.tags[5].key},
+						{value: this.tags[6].value, name: this.tags[6].key},
+						{value: this.tags[7].value, name: this.tags[7].key},
+						{value: this.tags[8].value, name: this.tags[8].key},
+						{value: this.tags[9].value, name: this.tags[9].key},
+					],
+					emphasis: {
+						itemStyle: {
+							shadowBlur: 10,
+							shadowOffsetX: 0,
+							shadowColor: 'rgba(0, 0, 0, 0.5)'
+						}
+					}
+				}
+			]
 		}
 	}
 }
